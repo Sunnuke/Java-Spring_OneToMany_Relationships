56e213787dd9a6234342f1b53e08ba2027f8e662
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-13 19:20:18 -0800

    Added spring-boot-devtools, mysql-connector-java, spring-boot-starter-tomcat, spring-boot-starter-validation, tomcat-embed-jasper, and jstl dependencies to pom.xml

---
+++ onetomanyrelationships/src/main/java/com/practice/onetomanyrelationships/OnetomanyrelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.practice.onetomanyrelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetomanyrelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetomanyrelationshipsApplication.class, args);
+	}
+
+}
